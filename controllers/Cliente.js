0c79cf22d2cb1f0331798fe711a05f7e979316ff
Edward Brito Diaz <37183622+Edwardb11@users.noreply.github.com>
2022-07-19 11:36:59 -0400

    update refresh token

--- controllers/Cliente.js
+++ controllers/Cliente.js
@@ -65,7 +65,7 @@ export const Login = async (req, res) => {
       { refresh_token: refreshToken },
       {
         where: {
-          id_clientes: userId,
+          correo_electronico: email,
         },
       }
     );
