36cbd28fd15a7a8affcc0cb4f470ff28ff8c8566
Edward Brito Diaz <37183622+Edwardb11@users.noreply.github.com>
2022-09-19 15:10:42 -0400

    feat: location

--- controllers/package.js
+++ controllers/package.js
@@ -8,8 +8,15 @@ import StaffModel from "../models/staffModel.js";
 import StateModel from "../models/statesModel.js";
 
 export const Package = async (req, res) => {
-  const { id_paquete, id_cliente, id_usuario_final, nombre, peso, cantidad } =
-    req.body;
+  const {
+    id_paquete,
+    id_cliente,
+    id_usuario_final,
+    nombre,
+    peso,
+    cantidad,
+    ubicacion,
+  } = req.body;
   console.log(req.body);
   try {
     await PackageModel.create({
@@ -19,6 +26,7 @@ export const Package = async (req, res) => {
       nombre: nombre,
       peso: peso,
       cantidad: cantidad,
+      ubicacion: ubicacion,
     });
     res.json({ msg: "Paquete registrado exitoxamente" });
   } catch (error) {
