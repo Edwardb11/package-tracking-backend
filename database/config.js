493876139e450396d64b0c13822406dbd5e6fdb0
Edward Brito Diaz <37183622+Edwardb11@users.noreply.github.com>
2022-11-22 12:57:19 -0400

    Fix: env

--- database/config.js
+++ database/config.js
@@ -1,7 +1,7 @@
 export const PORT = process.env.PORT || 5000;
 
-export const DB_HOST = process.env.DB_HOST || "localhost";
-export const DB_USER = process.env.DB_USER || "root";
-export const DB_PASSWORD = process.env.DB_HOST || "";
-export const DB_NAME = process.env.DB_NAME || "sistema_paqueteriaV2";
-export const DB_PORT = process.env.DB_PORT || 3306;
+export const MYSQLHOST = env.MYSQLHOST || "localhost";
+export const MYSQLUSER = env.MYSQLUSER || "root";
+export const MYSQLPASSWORD = env.MYSQLPASSWORD || "";
+export const MYSQLDATABASE = env.MYSQLDATABASE || "sistema_paqueteriaV2";
+export const MYSQLPORT = env.MYSQLPORT || 3306;
