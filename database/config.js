1c5032fb1248389b8f38d5989f07700bf1d9d5c5
Edward Brito Diaz <37183622+Edwardb11@users.noreply.github.com>
2022-11-22 12:28:56 -0400

    FIx

--- database/config.js
+++ database/config.js
@@ -1,4 +1,4 @@
-export const PORT = process.env.PORT || 3000;
+export const PORT = process.env.PORT || 5000;
 
 export const DB_HOST = process.env.DB_HOST || "localhost";
 export const DB_USER = process.env.DB_USER || "root";
