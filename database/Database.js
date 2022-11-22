9ef9974306eb1fb83577a4a1565742a1cf1bbd72
Edward Brito Diaz <37183622+Edwardb11@users.noreply.github.com>
2022-11-22 13:03:18 -0400

    :fix: variables

--- database/Database.js
+++ database/Database.js
@@ -1,9 +1,15 @@
 import { Sequelize } from "sequelize";
-import { DB_HOST, DB_NAME, DB_PASSWORD, DB_PORT, DB_USER } from "./config.js";
-const db = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
-  host: DB_HOST,
+import {
+  MYSQLHOST,
+  MYSQLDATABASE,
+  MYSQLPASSWORD,
+  MYSQLPORT,
+  MYSQLUSER,
+} from "./config.js";
+const db = new Sequelize(MYSQLDATABASE, MYSQLUSER, MYSQLPASSWORD, {
+  host: MYSQLHOST,
   dialect: "mysql",
-  port: DB_PORT,
+  port: MYSQLPORT,
   define: {
     timestamps: false,
     freezeTableName: true,
