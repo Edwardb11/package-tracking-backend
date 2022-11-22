10abbffdc5663dcd2602fc0876014749838a72ab
Edward Brito Diaz <37183622+Edwardb11@users.noreply.github.com>
2022-11-22 13:39:54 -0400

    env

--- database/config.js
+++ database/config.js
@@ -1,8 +1,8 @@
 export const PORT = process.env.PORT || 5000;
 
-export const MYSQLHOST = process.env.MYSQLHOST || "localhost";
-export const MYSQLUSER = process.env.MYSQLUSER || "root";
-export const MYSQLPASSWORD = process.env.MYSQLPASSWORD || "";
+export const MYSQLHOST = process.env(MYSQLHOST) || "localhost";
+export const MYSQLUSER = process.env(MYSQLUSER) || "root";
+export const MYSQLPASSWORD = process.env(MYSQLPASSWORD) || "";
 export const MYSQLDATABASE =
-  process.env.MYSQLDATABASE || "sistema_paqueteriaV2";
-export const MYSQLPORT = process.env.MYSQLPORT || 3306;
+  process.env(MYSQLDATABASE) || "sistema_paqueteriaV2";
+export const MYSQLPORT = process.env(MYSQLPORT) || 3306;
